822310517693b0784ba1d84572848d2a12acb01a
AnkitaKondala <ankita.stacy2002@gmail.com>
2026-09-22 13:56:55 +0530

    Resilience4j added - timeout, retry, circuit breaker, fallout

--- booking-service/src/main/java/com/stacy/bookingservice/client/EventServiceClient.java
+++ booking-service/src/main/java/com/stacy/bookingservice/client/EventServiceClient.java
@@ -1,12 +1,17 @@
 package com.stacy.bookingservice.client;
 
 import com.stacy.bookingservice.dto.EventResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Duration;
 
 @Component
 public class EventServiceClient {
@@ -14,8 +19,15 @@ public class EventServiceClient {
     private final RestClient restClient;
 
     public EventServiceClient(@Value("${event-service.url}") String baseUrl) {
+
+        var httpClient = java.net.http.HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(2)).build();
+
+        var requestFactory = new JdkClientHttpRequestFactory(httpClient);
+        requestFactory.setReadTimeout(Duration.ofSeconds(3));
+
         this.restClient = RestClient.builder()
                 .baseUrl(baseUrl)
+                .requestFactory(requestFactory)
                 .build();
     }
 
@@ -32,6 +44,8 @@ public class EventServiceClient {
         }
     }
 
+    @Retry(name="eventService")
+    @CircuitBreaker(name = "eventService",fallbackMethod = "reserveSeatsFallback")
     public EventResponse reserveSeats(Long eventId,int numberOfSeats) {
 
         try {
@@ -46,5 +60,16 @@ public class EventServiceClient {
         } catch (HttpClientErrorException.NotFound ex) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Event not found");
         }
+    }
+
+    private EventResponse reserveSeatsFallback(
+            Long eventId,
+            int numberOfSeats,
+            Throwable throwable) {
+
+        throw new ResponseStatusException(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                "Event Service is currently unavailable. Please try again later."
+        );
     }
 }
